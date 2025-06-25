1f297217805a90742b89763be529ac8657fb899b
Saman Devendra Pandey <singularity.1@hotmail.com>
2025-06-25 19:29:07 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,5 +3,5 @@ import React from 'react';
 import MobileFlashcardGenerator from './MobileFlashcardGenerator';
 
 const App = () => <MobileFlashcardGenerator />;
-
+console.log("API_URL:", process.env.REACT_APP_API_URL);
 export default App;
